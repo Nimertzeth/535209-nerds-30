7cce16c70d81919318e7008c24ae97d54992352f
Nimertzeth <77589721+Nimertzeth@users.noreply.github.com>
2021-02-21 13:18:51 +0300

    начало верстки

---
+++ script.js
@@ -0,0 +1,25 @@
+
+		function myFunction1() {
+ 		var x = document.getElementById("slaid1");
+ 		var z = document.getElementById("slaid2");
+ 		var w = document.getElementById("slaid3");
+ 		z.style.display = "none";
+ 		w.style.display = "none";
+ 		x.style.display = "grid"; 
+		}
+		function myFunction2() {
+ 		var x = document.getElementById("slaid2");
+ 		var z = document.getElementById("slaid1");
+ 		var w = document.getElementById("slaid3");
+ 		x.style.display = "grid"; 
+ 		z.style.display = "none";
+ 		w.style.display = "none";
+		}
+ 		function myFunction3() {
+ 		var x = document.getElementById("slaid3");
+ 		var z = document.getElementById("slaid2"); 
+ 		var w = document.getElementById("slaid1");
+ 		x.style.display = "grid"; 
+ 		w.style.display = "none";
+ 		z.style.display = "none";
+		}
